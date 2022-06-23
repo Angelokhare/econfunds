3320ef3473449be2b2d238dc9f2b25a98314be1b
Angelokhare <angelobeckan794@gmail.com>
2022-06-23 22:27:41 +0100

    ll

--- index.js
+++ index.js
@@ -16,38 +16,7 @@ app.get("/", (request, response)=>{
     var day=new Date().getFullYear()
     response.render("index", {fan:day})})
 
-app.get("/home", (request, response)=>{
-
-
-const options = {
-	"method": "GET",
-	"hostname": "coingecko.p.rapidapi.com",
-	"port": null,
-	"path": "/coins/markets?vs_currency=usd&page=1&per_page=100&order=market_cap_desc",
-	"headers": {
-		"X-RapidAPI-Key": "3c87ec6a25msh46b7d04fc169e7dp1cc42djsnd64003f09b54",
-		"X-RapidAPI-Host": "coingecko.p.rapidapi.com",
-		"useQueryString": true
-	}
-};
-
-const req = http.request(options, function (res) {
-	const chunks = [];
-
-	res.on("data", function (chunk) {
-		chunks.push(chunk);
-	});
-
-	res.on("end", function (request, response) {
-		const body = Buffer.concat(chunks);
-		// console.log(body.toString());
-        // var dis= body.toString()
-        response.render("home", {dis:dis})
-	});
-});
-
-req.end();
-})    
+app.get("/home", (request, response)=>{response.send("Anticipate")})   
 
 app.get("/signup", (request, response)=>{response.render("signup")})
 
