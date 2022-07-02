16419c3452cf09f0dec4d255d450916b978b11bd
Angelokhare <angelobeckan794@gmail.com>
2022-07-02 02:01:32 +0100

    sesses

--- index.js
+++ index.js
@@ -16,42 +16,68 @@ app.get("/", (request, response)=>{
     var day=new Date().getFullYear()
     response.render("index", {fan:day})})
 
+  var qq=""
 app.get("/home", (request, response)=>{
+
     var day=new Date().getFullYear()
-    // for(let i=1; i<251; i++){
+    const axios = require("axios");
+
     const options = {
-        "method": "GET",
-        "hostname": "coingecko.p.rapidapi.com",
-        "port": null,
-        "path": "/coins/markets?vs_currency=usd&page=1&per_page=100&order=market_cap_desc",
-        "headers": {
-            "X-RapidAPI-Key": "3c87ec6a25msh46b7d04fc169e7dp1cc42djsnd64003f09b54",
-            "X-RapidAPI-Host": "coingecko.p.rapidapi.com",
-            "useQueryString": true
-        }
+      method: 'GET',
+      url: 'https://coingecko.p.rapidapi.com/coins/markets',
+      params: {vs_currency: 'usd', page: '1', per_page: '250', order: 'market_cap_desc'},
+      headers: {
+        'X-RapidAPI-Key': '3c87ec6a25msh46b7d04fc169e7dp1cc42djsnd64003f09b54',
+        'X-RapidAPI-Host': 'coingecko.p.rapidapi.com'
+      }
     };
-    const req = http.request(options, function (res) {
-        const chunks = [];
     
-        res.on("data", function (chunk) {
-            chunks.push(chunk);
-        });
-    
-        res.on("end", function () {
-            const body = Buffer.concat(chunks);
-            // console.log(body.toString());
-            // var dave=body.toString()
-            // response.render("home", {fd:dave})
-            response.render("home", {fan:day})
+    axios.request(options).then(function (res) {
+        // console.log(res.data);
+        var hj= res.data
+        // console.log(hj)
+        qq = hj
+        // response.render("home", {fan:day, fact:hj})
+    }).catch(function (error) {
+        console.error(error);
     });
-    }
-    );
+    console.log(qq)
+    response.render("home", {fan:day, fact:qq})
+        // const options = {
+        //     "method": "GET",
+        //     "hostname": "coingecko.p.rapidapi.com",
+        //     "port": null,
+        //     "path": "/coins/markets?vs_currency=usd&page=1&per_page=100&order=market_cap_desc",
+        //     "headers": {
+        //         "X-RapidAPI-Key": "3c87ec6a25msh46b7d04fc169e7dp1cc42djsnd64003f09b54",
+        //         "X-RapidAPI-Host": "coingecko.p.rapidapi.com",
+        //         "useQueryString": true
+        //     }
+        // };
+        
+        // const req = http.request(options, function (res) {
+        //     const chunks = [];
+        
+        //     res.on("data", function (chunk) {
+        //         chunks.push(chunk);
+        //     });
+        
+        //     res.on("end", function () {
+        //         const body = Buffer.concat(chunks);
+        //         // console.log(body.toString());
+        //        var yt= body.toString()
+        //        console.log(yt[2])
     
-    req.end();
-// }
+        //        response.render("home", {fan:day, fact:body.toString()})
+        //     });
+        // });
+        // req.end();
+    
 
-})   
-
+    })
+    //    var hj= [{"hgyfy": "ff3tt"}, {"hgyfy": "fftt4"},{"hgyfy": "ff5tt"},{"hgyfy": "fftt"}]
+    //    console.log(hj[1])
+    
 app.get("/signup", (request, response)=>{response.render("signup")})
 app.get("/convert", (request, response)=>{response.render("convert")})
 
